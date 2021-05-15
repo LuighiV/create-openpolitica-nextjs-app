5f3f330c67757c7efc896afc8ac533eeef87367c
Luighi <luighiavz@gmail.com>
2021-05-15 00:42:15 -0500

    Modify default template for open-politica projects
    - Adds settings for developers (ESLint, Prettier, Husky)
    - Adds example at index.js with a simple header
    - Restructure public directory (images/favicon sub-directories)
    - Adds settings for compiler (jsconfig.json)
    - Adds settings to import SVG (next.config.js)
    - Updates README-template.md with instructions to import this template

---
+++ templates/default/components/Header/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled('h1')`
+  font-size: 2rem;
+`;
+
+export const LogoImg = styled.img`
+  cursor: pointer;
+  max-width: 200px;
+`;
