02665429b9f56ffb737b7c8eb042ad2216108a1a
Luighi <luighiavz@gmail.com>
2021-05-17 18:35:37 -0500

    Add configurations to styled-components server-side-rendering
    - Add settings in _document.js according to the examples at nextjs for
      styled-components package
    - Add theme provider for ssr
    - Add example of use of theme in Header component
    - Move icon reference to _app.js

--- templates/default/pages/_app.js
+++ templates/default/pages/_app.js
@@ -1,14 +1,25 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import { ThemeProvider } from "styled-components";
+import theme from "global-styles/theme";
+import GlobalStyle from "global-styles/global";
+import Head from "next/head";
 
 function MyOPApp({ Component, pageProps }) {
   return (
-    <Fragment>
+    <>
       <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+          href="favicon/favicon.ico"
+        />
         <title>My Open Politica App</title>
       </Head>
-      <Component {...pageProps} />
-    </Fragment>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
